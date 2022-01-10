938bb2dcab6b99a360eb72e61ebc79d3b6aa2edc
FiremnAkmal27 <91707566+FiremnAkmal27@users.noreply.github.com>
2022-01-11 01:18:57 +0700

    Login Dan Main Program

--- app/src/main/java/com/example/crud/Login.kt
+++ app/src/main/java/com/example/crud/Login.kt
@@ -2,10 +2,87 @@ package com.example.crud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
+import com.google.firebase.database.*
 
-class Login : AppCompatActivity() {
+class Login : AppCompatActivity(), View.OnClickListener {
+    private lateinit var etNama : EditText
+    private lateinit var etAlamat : EditText
+    private lateinit var btnSave : Button
+    private lateinit var listMhs : ListView
+    private lateinit var ref: DatabaseReference
+    private lateinit var mhsList: MutableList<Mahasiswa>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+
+        ref = FirebaseDatabase.getInstance().getReference("Crud")
+
+        etNama = findViewById(R.id.et_nama)
+        etAlamat = findViewById(R.id.et_alamat)
+        btnSave = findViewById(R.id.btn_save)
+        listMhs = findViewById(R.id.lv_mhs)
+        btnSave.setOnClickListener(this)
+
+        mhsList = mutableListOf()
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists()){
+                    mhsList.clear()
+                    for(h in p0.children){
+                        val mahasiswa = h.getValue(Mahasiswa::class.java)
+                        if (mahasiswa != null) {
+                            mhsList.add(mahasiswa)
+                        }
+                    }
+                    val adapter = MahasiswaAdapter(this@Login, R.layout.item_mhs, mhsList)
+                    listMhs.adapter = adapter
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
     }
+
+    override fun onClick(p0: View?) {
+        savedata()
+    }
+
+    private fun savedata(){
+        val nama : String = etNama.text.toString().trim()
+        val alamat : String = etAlamat.text.toString().trim()
+
+        if (nama.isEmpty()){
+            etNama.error = "isi nama"
+            return
+        }
+
+        if (alamat.isEmpty()){
+            etAlamat.error = "isi password"
+            return
+        }
+
+
+        val mhsId= ref.push().key
+
+        val mhs = Mahasiswa(mhsId!!,nama,alamat)
+
+        if (mhsId != null) {
+            ref.child(mhsId).setValue(mhs).addOnCompleteListener{
+                Toast.makeText(applicationContext, "Data berhasil ditambahkan", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/crud/MainActivity.kt
+++ app/src/main/java/com/example/crud/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.crud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,78 +11,19 @@ import android.widget.Toast
 import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var etNama : EditText
-    private lateinit var etAlamat : EditText
-    private lateinit var btnSave : Button
-    private lateinit var listMhs : ListView
-    private lateinit var ref: DatabaseReference
-    private lateinit var mhsList: MutableList<Mahasiswa>
+    private lateinit var btnLgn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
-        ref = FirebaseDatabase.getInstance().getReference("Crud")
-
-        etNama = findViewById(R.id.et_nama)
-        etAlamat = findViewById(R.id.et_alamat)
-        btnSave = findViewById(R.id.btn_save)
-        listMhs = findViewById(R.id.lv_mhs)
-        btnSave.setOnClickListener(this)
-
-        mhsList = mutableListOf()
-
-        ref.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(p0: DataSnapshot) {
-                if(p0.exists()){
-                    mhsList.clear()
-                    for(h in p0.children){
-                        val mahasiswa = h.getValue(Mahasiswa::class.java)
-                        if (mahasiswa != null) {
-                            mhsList.add(mahasiswa)
-                        }
-                    }
-                    val adapter = MahasiswaAdapter(this@MainActivity, R.layout.item_mhs, mhsList)
-                    listMhs.adapter = adapter
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-
-        })
-
+        btnLgn = findViewById(R.id.btnLgn)
+        btnLgn.setOnClickListener(this)
     }
 
-    override fun onClick(p0: View?) {
-        savedata()
-    }
+    override fun onClick(v: View?) {
+        val dashboardpage = Intent(this, Login::class.java)
 
-    private fun savedata(){
-        val nama : String = etNama.text.toString().trim()
-        val alamat : String = etAlamat.text.toString().trim()
-
-        if (nama.isEmpty()){
-            etNama.error = "isi nama"
-            return
-        }
-
-        if (alamat.isEmpty()){
-            etAlamat.error = "isi password"
-            return
-        }
-
-
-        val mhsId= ref.push().key
-
-        val mhs = Mahasiswa(mhsId!!,nama,alamat)
-
-        if (mhsId != null) {
-            ref.child(mhsId).setValue(mhs).addOnCompleteListener{
-                Toast.makeText(applicationContext, "Data berhasil ditambahkan", Toast.LENGTH_SHORT).show()
-            }
-        }
-
+        startActivity(dashboardpage)
     }
 }
