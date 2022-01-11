b6119f2e41ab9a3f1c6b0a0ad9a9273c7bedcc40
FiremnAkmal27 <91707566+FiremnAkmal27@users.noreply.github.com>
2022-01-11 07:08:10 +0700

    Done

--- app/src/main/java/com/example/crud/DashMenu.kt
+++ app/src/main/java/com/example/crud/DashMenu.kt
@@ -1,11 +1,26 @@
 package com.example.crud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.cardview.widget.CardView
 
-class DashMenu : AppCompatActivity() {
+class DashMenu : AppCompatActivity(), View.OnClickListener {
+    private lateinit var card : CardView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_menu)
+
+        card=findViewById(R.id.cardHutang)
+        card.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        val dashhome= Intent(this, Login::class.java)
+
+        startActivity(dashhome)
     }
 }
--- app/src/main/java/com/example/crud/MainActivity.kt
+++ app/src/main/java/com/example/crud/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        val dashboardpage = Intent(this, Login::class.java)
+        val dashboardpage = Intent(this, DashMenu::class.java)
 
         startActivity(dashboardpage)
     }
